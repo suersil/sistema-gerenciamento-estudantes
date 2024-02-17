8093ebec5f2e66102049dfca90dc87dc6fbda4dd
Priscilla K <priscillakleine@gmail.com>
2024-02-17 18:20:53 -0300

    teste inicial

--- gerenciamentoEstudantes/src/main/java/tech/ada/java/gerenciamento/estudantes/gerenciamentoestudantes/Repository/RepositorioProfessor.java
+++
@@ -1,7 +0,0 @@
-package tech.ada.java.gerenciamento.estudantes.gerenciamentoestudantes.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.ada.java.gerenciamento.estudantes.gerenciamentoestudantes.Model.Professor;
-
-public interface RepositorioProfessor extends JpaRepository<Professor, Long> {
-}
--- gerenciamentoEstudantes/src/main/java/tech/ada/java/gerenciamento/estudantes/gerenciamentoestudantes/Repository/RepositorioTurma.java
+++
@@ -1,7 +0,0 @@
-package tech.ada.java.gerenciamento.estudantes.gerenciamentoestudantes.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.ada.java.gerenciamento.estudantes.gerenciamentoestudantes.Model.Turma;
-
-public interface RepositorioTurma extends JpaRepository<Turma, Long> {
-}
