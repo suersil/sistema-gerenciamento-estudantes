1ca03175f8cbee91c73a49bca7940f893c13eafd
Natália Diotto <diottonatalia@gmail.com>
2024-02-18 11:34:44 -0300

    Implementando metodo para cadastrar Professor

--- gerenciamentoEstudantes/src/main/java/tech/ada/java/gerenciamento/estudantes/gerenciamentoestudantes/GerenciamentoEstudantesApplication.java
+++ gerenciamentoEstudantes/src/main/java/tech/ada/java/gerenciamento/estudantes/gerenciamentoestudantes/GerenciamentoEstudantesApplication.java
@@ -1,13 +1,20 @@
 package tech.ada.java.gerenciamento.estudantes.gerenciamentoestudantes;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GerenciamentoEstudantesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GerenciamentoEstudantesApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
