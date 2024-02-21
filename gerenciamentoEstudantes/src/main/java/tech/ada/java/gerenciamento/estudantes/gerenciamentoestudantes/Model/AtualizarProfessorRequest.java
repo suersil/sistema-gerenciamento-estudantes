bec322a12212c66a18ccfe040a774f25fe648271
suersil <suelenregsilva@gmail.com>
2024-02-21 19:02:08 -0300

    fixing

--- gerenciamentoEstudantes/src/main/java/tech/ada/java/gerenciamento/estudantes/gerenciamentoestudantes/Model/AtualizarProfessorRequest.java
+++ gerenciamentoEstudantes/src/main/java/tech/ada/java/gerenciamento/estudantes/gerenciamentoestudantes/Model/AtualizarProfessorRequest.java
@@ -2,12 +2,13 @@ package tech.ada.java.gerenciamento.estudantes.gerenciamentoestudantes.Model;
 
 import java.util.Objects;
 
-public record AtualizarProfessorRequest(String nomeProfessor, String email, String disciplinaLecionada){
+public record AtualizarProfessorRequest(String nomeProfessor, String email, String disciplinaLecionada, boolean estaAtivo){
 
     //Transformar a request em entidade verificando se os atributos nao sao nulos
-    public AtualizarProfessorRequest(String nomeProfessor, String email, String disciplinaLecionada) {
+    public AtualizarProfessorRequest(String nomeProfessor, String email, String disciplinaLecionada, boolean estaAtivo) {
         this.nomeProfessor = Objects.requireNonNull(nomeProfessor, "nomeProfessor é obrigatório");
         this.email = Objects.requireNonNull(email, "email é obrigatório");
         this.disciplinaLecionada = Objects.requireNonNull(disciplinaLecionada, "disciplinaLecionada é obrigatória");
+        this.estaAtivo = Objects.requireNonNull(estaAtivo);
     }
 }
