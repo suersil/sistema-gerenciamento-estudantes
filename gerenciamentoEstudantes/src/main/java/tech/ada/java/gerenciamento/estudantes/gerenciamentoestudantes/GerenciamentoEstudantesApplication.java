e23fe5e291b1c9156bff08a340967074cb11de36
suersil <suelenregsilva@gmail.com>
2024-02-23 16:09:20 -0300

    Adicionando validacao ao criar e editar Turma - Jakarta

--- gerenciamentoEstudantes/src/main/java/tech/ada/java/gerenciamento/estudantes/gerenciamentoestudantes/GerenciamentoEstudantesApplication.java
+++ gerenciamentoEstudantes/src/main/java/tech/ada/java/gerenciamento/estudantes/gerenciamentoestudantes/GerenciamentoEstudantesApplication.java
@@ -4,14 +4,18 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 @SpringBootApplication
 public class GerenciamentoEstudantesApplication {
     public static void main(String[] args) {
         SpringApplication.run(GerenciamentoEstudantesApplication.class, args);
     }
+
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
 }
