46aedc2170686d420b62d06ad8cb9a089dd989d6
yelalopez <yelitzalopezduque@gmail.com>
2024-03-14 13:11:38 -0300

    test: findTurmaByEstaAtiva com sucesso e com exception

--- gerenciamentoEstudantes/src/test/java/tech/ada/java/gerenciamento/estudantes/gerenciamentoestudantes/Service/ServiceTurmaTest.java
+++ gerenciamentoEstudantes/src/test/java/tech/ada/java/gerenciamento/estudantes/gerenciamentoestudantes/Service/ServiceTurmaTest.java
@@ -8,10 +8,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import tech.ada.java.gerenciamento.estudantes.gerenciamentoestudantes.DTOS.TurmaDTO;
+import tech.ada.java.gerenciamento.estudantes.gerenciamentoestudantes.Errors.ResourceNotFoundException;
 import tech.ada.java.gerenciamento.estudantes.gerenciamentoestudantes.Model.AlterarTurmaRequest;
 import tech.ada.java.gerenciamento.estudantes.gerenciamentoestudantes.Model.Turma;
 import tech.ada.java.gerenciamento.estudantes.gerenciamentoestudantes.Repository.RepositorioTurma;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,8 +35,8 @@ class ServiceTurmaTest {
 
     Turma turma;
     TurmaDTO turmaDTO;
-
     AlterarTurmaRequest turmaRequest;
+    List<Turma> listaTurma;
 
     String nomeTurma= "5oAnoC";
     Boolean estaAtiva = true;
@@ -73,12 +76,57 @@ class ServiceTurmaTest {
         verify(repositorioTurma, times(1)).save(any());
         verifyNoMoreInteractions(repositorioTurma);
     }
-
     @Test
-    void alteraTurmaCompleto() {
+    void alterarTurmaComException(){
+//        when(repositorioTurma.findById(anyLong())).thenReturn(Optional.empty());
+//        when(repositorioTurma.save(any())).thenReturn(turma);
+//
+//        assertThrows(ResourceNotFoundException.class, () -> {
+//            serviceTurma.alterarTurma(1L, turmaRequest);
+//        });
+//
+//        verify(repositorioTurma, times(1)).findById(1L);
+//        verifyNoMoreInteractions(repositorioTurma);
     }
 
     @Test
-    void findTurmaByEstaAtiva() {
+    void alteraTurmaCompleto() {
+        when(repositorioTurma.findById(anyLong())).thenReturn(Optional.of(turma));
+        when(repositorioTurma.save(any())).thenReturn(turma);
+
+        Turma turmaAtualizada = serviceTurma.alteraTurmaCompleto(1L, turmaRequest);
+
+        assertNotNull(turmaAtualizada);
+        assertEquals(estaAtiva, turmaAtualizada.getEstaAtiva());
+        assertEquals(nomeTurma, turmaAtualizada.getNomeTurma());
+
+        verify(repositorioTurma, times(1)).findById(1L);
+        verify(repositorioTurma, times(1)).save(any());
+        verifyNoMoreInteractions(repositorioTurma);
+    }
+
+    @Test
+    void findTurmaByEstaAtivaComSucesso() throws Exception{
+        when(repositorioTurma.findTurmaByEstaAtiva(true)).thenReturn(List.of(turma));
+        List<Turma> turmaFiltrada = serviceTurma.findTurmaByEstaAtiva(true);
+
+        assertNotNull(turmaFiltrada);
+        assertEquals(1, turmaFiltrada.size());
+        assertFalse(turmaFiltrada.isEmpty());
+
+        verify(repositorioTurma, times(1)).findTurmaByEstaAtiva(true);
+        verifyNoMoreInteractions(repositorioTurma);
+    }
+
+    @Test
+    void findTurmaByEstaAtivaComException(){
+        when(repositorioTurma.findTurmaByEstaAtiva(true)).thenReturn(Collections.emptyList());
+
+        assertThrows(ResourceNotFoundException.class, () ->{
+            serviceTurma.findTurmaByEstaAtiva(true);
+        });
+
+        verify(repositorioTurma, times(1)).findTurmaByEstaAtiva(true);
+        verifyNoMoreInteractions(repositorioTurma);
     }
 }
