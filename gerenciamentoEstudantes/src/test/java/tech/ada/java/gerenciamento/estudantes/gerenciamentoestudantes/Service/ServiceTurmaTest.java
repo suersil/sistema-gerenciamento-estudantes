e6c4bd607f67d731cf9cf3db88c5638a080bb8ea
Priscilla K <priscillakleine@gmail.com>
2024-03-14 15:22:11 -0300

    testes cadastrar e buscar (com sucesso) ServiceTurma implementado

--- gerenciamentoEstudantes/src/test/java/tech/ada/java/gerenciamento/estudantes/gerenciamentoestudantes/Service/ServiceTurmaTest.java
+++ gerenciamentoEstudantes/src/test/java/tech/ada/java/gerenciamento/estudantes/gerenciamentoestudantes/Service/ServiceTurmaTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import tech.ada.java.gerenciamento.estudantes.gerenciamentoestudantes.DTOS.TurmaDTO;
+import tech.ada.java.gerenciamento.estudantes.gerenciamentoestudantes.Model.Professor;
 import tech.ada.java.gerenciamento.estudantes.gerenciamentoestudantes.Model.Turma;
 import tech.ada.java.gerenciamento.estudantes.gerenciamentoestudantes.Repository.RepositorioTurma;
 
@@ -17,7 +18,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ServiceTurmaTest {
@@ -44,10 +45,32 @@ class ServiceTurmaTest {
 
     @Test
     void cadastrarTurma() {
+        when(repositorioTurma.save(any())).thenReturn(turmaDTO.toEntity());
+        Turma turmaCadastrada = serviceTurma.cadastrarTurma(turmaDTO);
+        assertNotNull(turmaCadastrada);
+        assertEquals(nomeTurma, turmaCadastrada.getNomeTurma());
+        assertEquals(estaAtiva, turmaCadastrada.getEstaAtiva());
+        verifyNoMoreInteractions(repositorioTurma);
     }
 
     @Test
     void buscarTurmas() {
+        //when: config do mock, metodo que é chamado dentro do metodo de teste
+        //thenReturn: o que esperamos ser retornado quando o metodo é chamado
+        when(repositorioTurma.findAll()).thenReturn(List.of(turma));
+        List<Turma> listaTurmas = serviceTurma.buscarTurmas();
+
+        //assertions
+        assertNotNull(listaTurmas); //assegurar que a lista nao é nula
+        assertEquals("Turma", listaTurmas.get(0).getClass().getSimpleName());// Verifica o tipo do primeiro elemento da lista
+        assertEquals(1, listaTurmas.size()); //assegurar que tamanho da lista esperado com o obtido/atual
+        assertEquals(Turma.class, listaTurmas.get(0).getClass()); //assegurar que o objeto esperado é do mesmo tipo que o obj obtido
+
+        // Verifica se o método findAll foi chamado apenas uma vez
+        verify(repositorioTurma).findAll();
+        // Verifica se não houve mais interações com o mock repositorioTurma
+        verifyNoMoreInteractions(repositorioTurma);
+
     }
 
     @Test
